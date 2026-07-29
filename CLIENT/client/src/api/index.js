ddd971f1801e6bb3743abee4992be2b4549cb348
fatikabrar60-rgb <fatikabrar60@gmail.com>
2026-07-29 12:13:44 +0500

    Add frontend deployment configuration

--- CLIENT/client/src/api/index.js
+++ CLIENT/client/src/api/index.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL: "https://ai-image-generator-mern-app.onrender.com/api/",
+  
 });
 
 export const GetPosts = async () => await API.get("/post");
